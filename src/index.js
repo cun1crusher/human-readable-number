43d3fb3e48449af19ac2ffe7df80dd2309a290a1
cun1crusher <superhanty@gmail.com>
2021-03-15 20:36:33 +0300

    add: human readable number solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const digits = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight',
+        'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+        'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let numStr = number.toString();
+
+    if (number === 0) {
+        return 'zero';
+    }
+
+    if (number < 20) {
+        return digits[number];
+    }
+
+    if (numStr.length == 2) {
+        return (tens[numStr[0]] + ' ' + digits[numStr[1]]).trim();
+    }
+
+    if (numStr.length == 3) {
+        if (numStr[1] == '0' && numStr[2] == '0') {
+            return digits[numStr[0]] + ' hundred';
+        }
+        else {
+            return digits[numStr[0]] + ' hundred ' + toReadable(+(numStr[1] + numStr[2]));
+        }
+    }
 }
